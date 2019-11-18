b468da4759579c74c4e1c188bc3f3be358437481
Mark <mverleg.noreply@gmail.com>
2019-11-18 20:59:30 +0100

    Matrix init code

--- tryjni/src/main/java/tryjni/App.java
+++ tryjni/src/main/java/tryjni/App.java
@@ -4,17 +4,32 @@ import javax.annotation.Nonnull;
 
 /**
  * Hello world!
- *
  */
 public class App {
     public static void main(@Nonnull String[] args) {
-        double[][][] matrix = new double[4][3][2];
+        make3D(4, 3, 2);
+        make2D(4, 3);
+    }
+
+    public static double[][] make2D(int a, int b) {
+        double[][]matrix = new double[a][b];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = 1 + b * i + j;
+            }
+        }
+        return matrix;
+    }
+
+    public static double[][][] make3D(int a, int b, int c) {
+        double[][][] matrix = new double[a][b][c];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 for (int k = 0; k < matrix[i][j].length; k++) {
-                    matrix[i][j][k] = i + j + k;
+                    matrix[i][j][k] = 1 + b * c * i + c * j + k;
                 }
             }
         }
+        return matrix;
     }
 }
