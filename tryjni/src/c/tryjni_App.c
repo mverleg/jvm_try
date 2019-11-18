4a7f2f8a65cbf8d45f69e6e0625177787c142c82
Mark <mverleg.noreply@gmail.com>
2019-11-18 23:10:00 +0100

    Midway to returning a new double array

--- tryjni/src/c/tryjni_App.c
+++ tryjni/src/c/tryjni_App.c
@@ -9,17 +9,33 @@ JNIEXPORT jobjectArray JNICALL Java_tryjni_App_mul22(JNIEnv *env, jclass _, jobj
     printf("hello from C!");
 
     jsize leftX = (*env)->GetArrayLength(env, left);
-
-    for (int i = 0; i < leftX; i++) {
-        jdoubleArray arr = (jdoubleArray) (*env)->GetObjectArrayElement(env, left, i);
-        jsize leftY = (*env)->GetArrayLength(env, arr);
-        jdouble* vals = (*env)->GetDoubleArrayElements(env, arr, NULL);
-
-        /* make changes to 'vals' */
-
-        (*env)->ReleaseDoubleArrayElements(env, arr, vals, JNI_COMMIT);
-        (*env)->DeleteLocalRef(env, arr);
+    jsize rightX = (*env)->GetArrayLength(env, right);
+    if (leftX <= 0 || rightX <= 0) {
+        printf("empty input");
+        return NULL;
     }
+    jdoubleArray leftRow = (jdoubleArray) (*env)->GetObjectArrayElement(env, left, 0);
+    jsize leftY = (*env)->GetArrayLength(env, leftRow);
+    jdoubleArray rightRow = (jdoubleArray) (*env)->GetObjectArrayElement(env, right, 0);
+    jsize rightY = (*env)->GetArrayLength(env, rightRow);
+
+    //TODO @mark: doesn't work yet: https://gamedev.stackexchange.com/a/96958/48705
+    jclass doubleArray2DClass = env->FindClass("[[D");
+    jobjectArray output = env->NewObjectArray(leftX, doubleArray2DClass, NULL);
+    for (int i = 0; i < leftX; i++) {
+        jdoubleArray row = env->NewObjectArray(rightY, jdoubleArray, NULL);
+    }
+
+//    for (int i = 0; i < leftX; i++) {
+//        jdoubleArray arr = (jdoubleArray) (*env)->GetObjectArrayElement(env, left, i);
+//        jsize leftY = (*env)->GetArrayLength(env, arr);
+//        jdouble* vals = (*env)->GetDoubleArrayElements(env, arr, NULL);
+//
+//        /* make changes to 'vals' */
+//
+//        (*env)->ReleaseDoubleArrayElements(env, arr, vals, JNI_COMMIT);
+//        (*env)->DeleteLocalRef(env, arr);
+//    }
 
     return NULL;
 }
